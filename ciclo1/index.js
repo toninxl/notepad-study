8a0bb6646206d769c6dd541b0b41344a3accfa0b
Antonio Santos <103208569+toninxl@users.noreply.github.com>
2026-05-01 18:29:45 +0000

    adicionado a função de salvar os dados em um arquivo json e adicone o gitignore do projeto

--- ciclo1/index.js
+++ ciclo1/index.js
@@ -1,4 +1,7 @@
 const readline = require('readline'); // 1. Importa
+const fs = require('fs');
+
+const caminhoArquivo = "dados.json"
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -38,10 +41,35 @@ async function consultarCep(cep) {
     
 }
 
+function salvarDados(dados) {
+    const uuid = crypto.randomUUID();
+    const dataToSave = dados
+    const dadosJson = {
+        idRequest : uuid,
+        dados: dataToSave
+    }
+
+    let listaDados = []; 
+    
+    if(fs.existsSync(caminhoArquivo)){
+        const conteudoArquivo = fs.readFileSync(caminhoArquivo, "utf-8")
+        
+        listaDados = JSON.parse(conteudoArquivo)
+    }
+
+    listaDados.push(dadosJson)
+    
+    console.log("Salvando dados:",dadosJson)
+    const jsonString = JSON.stringify(listaDados, null, 2);
+    fs.writeFileSync(caminhoArquivo, jsonString, 'utf-8');
+    
+}
+
 rl.question('Qual o seu CEP? ', async (resposta) => {
   const dados = await consultarCep(resposta);
   if(dados)
   console.log(dados)
+  salvarDados(dados)
   rl.close(); 
 });
 
